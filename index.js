de8fdd78ddae6d5384362bb1bb19e722ee3e1b6c
FM <fcmbox2@gmail.com>
2022-03-02 02:08:47 -0500

    renamed .projectscontainer to .projectsbadge. fleshed out skills css.

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ var typed = new Typed('#hello', options = {
 );
 function checkResIfSupport(){
     if(screen.width > 1920){
-        alert("Your resolution is currently unsupported on my website at a screen width of " + screen.width + "px. Some elements may not render or position correctly. Sorry about that I'm still working on it.");
+        alert("Your resolution is currently unsupported on my website at a screen width of " + screen.width + "\
+        px. Some elements may not render or position correctly. The largest currently supported width is 1920px. \
+        Sorry about that I'm still working on it.");
     }
 }
 checkResIfSupport();
