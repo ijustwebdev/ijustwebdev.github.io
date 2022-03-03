064482283007e56f83c17d8a3651df52ca3f5ce5
FM <fcmbox2@gmail.com>
2022-03-03 00:43:19 -0500

    removing warn

--- index.js
+++ index.js
@@ -17,7 +17,3 @@ function checkResIfSupport(){
     }
 }
 checkResIfSupport();
-function warn(){
-    alert("Some things broke when switching to github pages from live server. If you're here already somehow just bear with me while I fix this. Thanks.");
-}
-warn();
